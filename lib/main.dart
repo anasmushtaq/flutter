1a54f63d761ce18582664932f366fa4b4ebea5e4
anasmushtaq <61446799+anasmushtaq@users.noreply.github.com>
2021-05-05 11:15:36 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp (
+      home: Scaffold (
+        body: Text ('\n \n Name: Anus Mushtaq \n \n \n Email: anas1192@gmail.com'),
+      ),
+    );
+  }
+}
